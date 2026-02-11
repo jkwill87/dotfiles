dbff91b05056e8ebbf1328fabca8e141c78f3ff1
Jessy Williams <jessy@jessywilliams.com>
2026-02-11 12:14:17 -0500

    Add jq LSP and enable JSON formatting in jsonls

--- home/dot_config/nvim/lsp/jsonls.lua
+++ home/dot_config/nvim/lsp/jsonls.lua
@@ -2,4 +2,7 @@ return {
   cmd = { 'vscode-json-language-server', '--stdio' },
   filetypes = { 'json', 'jsonc' },
   root_markers = { '.git' },
+  init_options = {
+    provideFormatter = true,
+  },
 }
